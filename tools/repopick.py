4b4f8c0ac1f0380cc10a642ed3d2aa463f2c78e2
Ayysir <ayysir@aospal.com>
2014-09-21 23:05:35 -0400

    Repopick: remove legacy gerrit port
    Change-Id: Ib1a7be3399eaac9f24dd835dc003c4ebb7ac4856

--- tools/repopick.py
+++ tools/repopick.py
@@ -192,7 +192,7 @@ for change in args.change_number:
     # gerrit returns two lines, a magic string and then valid JSON:
     #   )]}'
     #   [ ... valid JSON ... ]
-    url = 'http://gerrit.aospal.com:8081/changes/?q=%s&o=CURRENT_REVISION&o=CURRENT_COMMIT&pp=0' % change
+    url = 'http://gerrit.aospal.com/changes/?q=%s&o=CURRENT_REVISION&o=CURRENT_COMMIT&pp=0' % change
     if args.verbose:
         print('Fetching from: %s\n' % url)
     f = urllib.request.urlopen(url)
